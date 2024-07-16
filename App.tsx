216cac61413907e1e2455069e91447d6308c7502
Pedro Luz <pedroluz895@gmail.com>
2024-07-16 16:54:01 -0300

    the first commit mobile

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/loading';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+      return (<Loading/>);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent ={true} />
     </View>
   );
 }
@@ -17,4 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'red'
+  }
 });
