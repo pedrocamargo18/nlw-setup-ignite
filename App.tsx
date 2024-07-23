a78e3122d8bae38d887ea255758e8af698fcc417
Pedro Luz <pedroluz895@gmail.com>
2024-07-23 15:51:56 -0300

    create and insert components in screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import './src/lib/dayjs';
-import React from 'react';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter';
 import { Home } from './src/screens/Home';
