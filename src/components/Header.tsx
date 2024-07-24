fbd52fb80da1f37fe1838f1b174c82fe6d7d31b7
Pedro Luz <pedroluz895@gmail.com>
2024-07-24 11:54:00 -0300

    fix CSS and frontEnd Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,14 @@ export function Header(){
     return (
         <View className="w-full flex-row items-center justify-between">
             <Logo />
-            <TouchableOpacity activeOpacity={0.7} className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
-            <Feather name="plus" color={colors.violet[500]} size={20} />
+            <TouchableOpacity activeOpacity={0.7} 
+            className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+            >
+                <Feather name="plus" color={colors.violet[500]} size={20} />
+                <Text className="text-white ml-3 font-semibold text-base">
+                    Novo
+                </Text>
             </TouchableOpacity>
-            <Text className="text-white ml-3 font-semibold text-base">
-
-            </Text>
         </View>
     )
 }
